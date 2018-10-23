4a89b73a99aff3b043cf1ace48cdcb86f8e2ed92
Raul Grimaldi <gemeraul94@gmail.com>
2018-10-22 19:37:44 -0600

    Adding new stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InventoryListComponent } from './components/inventory-list/inventory-list.component'
+import { AlquilerComponent } from './components/alquiler/alquiler.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'inventario', component: InventoryListComponent },
+  { path: 'alquiler', component: AlquilerComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,14 +11,19 @@ import { HeaderComponent } from './components/header/header.component';
 import {
   MatExpansionModule,
   MatFormFieldModule,
-  MatIconModule
+  MatIconModule,
+  MatStepperModule,
+  MatInputModule,
+  MatButtonModule
 } from '@angular/material';
+import { AlquilerComponent } from './components/alquiler/alquiler.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InventoryListComponent,
-    HeaderComponent
+    HeaderComponent,
+    AlquilerComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +32,12 @@ import {
     MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
-    BrowserAnimationsModule
+    MatStepperModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
